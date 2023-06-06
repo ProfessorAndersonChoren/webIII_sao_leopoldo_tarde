d52c357d8197d832098633e147068f5f90a56f15
Professor Choren <anderson.choren@faqi.edu.br>
2023-06-06 16:57:41 -0300

    Projeto Hello World

---
+++ hello_world/src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello, what's up guys. How your doing?");
+        System.out.print("We are studying Java Language programming!");
+    }
+}
